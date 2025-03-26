294c5346572776548b91d9d4465a23352402033d
Mahimul Islam <mahimul@maeknit.io>
2025-03-26 13:56:54 -0400

    Added GA Analytics Code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Work from "./pages/Work";
 import Onboarding from "./pages/Onboarding";
-import Draw from "./pages/Draw"; // Add this import
+import Draw from "./pages/Draw";
 
+// Initialize GA once
+ReactGA.initialize("G-JVQKEJ6TGH");
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Fire a pageview on route change
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+
   return (
     <div className="App">
       <div className="main-content">
@@ -21,7 +32,6 @@ function App() {
           <Route path="/onboarding" element={<Onboarding />} />
           <Route path="/work" element={<Work />} />
           <Route path="/draw" element={<Draw />} />
-
         </Routes>
       </div>
     </div>
